761a73326b98bea3782eb34c4a7a7c1257178e9f
eos.chen <eos.chen@nadisystem.com>
2023-05-02 19:13:00 +0800

    + 單元測試 - 製作 IFC Etree 當中

--- tests/UT_3_IFCETree_20230502_001_01.py
+++ tests/UT_3_IFCETree_20230502_001_01.py
@@ -1,5 +1,7 @@
 import bpy
 import os
+from math import radians
+from mathutils import Euler
 
 os.system('cls')
 
@@ -47,9 +49,9 @@ class BlenderOperator:
                     self.remove_object_from_collection(obj, col)
             self.add_object_to_collection(obj, parent_col)
 
-    def set_parent(self, parent, child) -> bpy.types.Object:
-        parent_obj = self.get_object_by_name(parent)
+    def set_parent(self, child, parent) -> bpy.types.Object:
         child_obj = self.get_object_by_name(child)
+        parent_obj = self.get_object_by_name(parent)
         if not parent_obj or not child_obj:
             return
         parent_col = self.get_collection_by_object(parent_obj)
@@ -65,11 +67,11 @@ class BlenderOperator:
     def set_parent_by_select(self) -> bpy.types.Object:
         selected_objects = bpy.context.selected_objects
         if len(selected_objects) < 2:
-            print("Please select at least 2 objects.")
+            print("[set_parent_by_select] Please select at least 2 objects.")
             return
         parent_obj = bpy.context.active_object
         if parent_obj not in selected_objects:
-            print("Please select the parent object.")
+            print("[set_parent_by_select] Please select the parent object.")
             return
         for obj in selected_objects:
             if obj != parent_obj:
@@ -93,7 +95,7 @@ class BlenderOperator:
                 traverse(child)
 
         if root_col not in bpy.data.collections:
-            print(f"Collection \"{root_col}\" not found.")
+            print(f"[reduce_ifc_objects] Collection \"{root_col}\" not found.")
             return
         traverse(bpy.data.collections.get(root_col))
 
@@ -107,13 +109,39 @@ class BlenderOperator:
         # print(f"\"{new_object.name}\" has been added.")
         return new_object
 
+    @staticmethod
+    def set_location(obj: str, location: tuple) -> None:
+        obj = bpy.data.objects.get(obj)
+        if not obj:
+            print(f"[set_location] Object {obj} not found")
+            return
+        obj.location = location
+
+    @staticmethod
+    def set_rotation(obj, rotation, use_degree=False):
+        obj = bpy.data.objects.get(obj)
+        if not obj:
+            print(f"[set_location] Object {obj} not found")
+            return
+        if use_degree:
+            rotation = tuple(radians(angle) for angle in rotation)
+        obj.rotation_euler = Euler(rotation)
+
+    @staticmethod
+    def set_scale(obj, scale):
+        obj = bpy.data.objects.get(obj)
+        if not obj:
+            print(f"[set_location] Object {obj} not found")
+            return
+        obj.scale = scale
+
 
 class IFCParser:
     def __init__(self, root="IfcBuilding/"):
         self.bl_root = bpy.data.collections.get(root)
         self.ifc_collections = [self.bl_root.name] + self.convert_to_names(self.bl_root.children_recursive)
         self.ifc_objects = self.bl_root.all_objects.keys()
-        self.ifc_elements = {}
+        self.ifc_info = {}
 
         self.is_executed = False
 
@@ -126,7 +154,7 @@ class IFCParser:
 
     def read_ifc(self):
         content = {}
-        self.ifc_elements[self.bl_root.name] = None
+        self.ifc_info[self.bl_root.name] = None
         for obj in self.ifc_objects:
             content["bl_type"] = self.define_bl_type(obj)
             content["ifc_parent"] = self.define_ifc_parent(obj)
@@ -135,9 +163,8 @@ class IFCParser:
             content["ifc_site"] = self.define_ifc_site(obj)
             content["ifc_building"] = self.define_ifc_building(obj)
             content["ifc_storey"] = self.define_ifc_storey(obj)
-            content["ifc_transform"] = self.define_ifc_transform(obj)
-            # TODO: et_path: root/高雄港埠旅運中心/15FL
-            self.ifc_elements[obj] = dict(content)
+            content["transform"] = self.define_transform(obj)
+            self.ifc_info[obj] = dict(content)
 
     def define_bl_type(self, obj):
         if obj in self.ifc_collections:
@@ -179,24 +206,24 @@ class IFCParser:
             return obj.split("/")[1]
 
     @staticmethod
-    def define_ifc_transform(obj):
+    def define_transform(obj):
         obj = bpy.data.objects[obj]
         transform = {
-            "position": [
-                obj.matrix_world.to_translation().x.__str__(),
-                obj.matrix_world.to_translation().y.__str__(),
-                obj.matrix_world.to_translation().z.__str__()
-            ],
-            "rotation": [
-                obj.matrix_world.to_euler().x.__str__(),
-                obj.matrix_world.to_euler().y.__str__(),
-                obj.matrix_world.to_euler().z.__str__()
-            ],
-            "scale": [
-                obj.matrix_world.to_scale().x.__str__(),
-                obj.matrix_world.to_scale().y.__str__(),
-                obj.matrix_world.to_scale().z.__str__()
-            ]
+            "position": (
+                obj.matrix_world.to_translation().x,
+                obj.matrix_world.to_translation().y,
+                obj.matrix_world.to_translation().z
+            ),
+            "rotation": (
+                obj.matrix_world.to_euler().x,
+                obj.matrix_world.to_euler().y,
+                obj.matrix_world.to_euler().z
+            ),
+            "scale": (
+                obj.matrix_world.to_scale().x,
+                obj.matrix_world.to_scale().y,
+                obj.matrix_world.to_scale().z
+            )
         }
         return transform
 
@@ -210,25 +237,36 @@ class IFCParser:
 class IFCBuilder:
     def __init__(self, parser: IFCParser):
         self.parser = parser
-        self.ifc_elements = parser.ifc_elements
-        self.instances = {}
+        self.ifc_info = parser.ifc_info
 
         self.is_executed = False
 
     def build(self):
-        if not self.ifc_elements:
+        if not self.ifc_info:
             print("IFC elements has not been parsed, would not build IFC objects.")
             return
-        for obj, elem in self.ifc_elements.items():
-            self.instances[obj] = BlenderOperator().add_object(obj).name
+        for obj, elem in self.ifc_info.items():
+            self.ifc_info[obj]["ifc_inst"] = BlenderOperator().add_object(obj).name
 
     def assemble(self):
-        for obj, inst in self.instances.items():
-            ifc_parent = self.ifc_elements[obj]["ifc_parent"]
+        for obj, ifc_info in self.ifc_info.items():
+            inst = self.ifc_info[obj]["ifc_inst"]
+            ifc_parent = self.ifc_info[obj]["ifc_parent"]
             if not ifc_parent:
                 continue
-            parent = self.instances[ifc_parent]
-            BlenderOperator().set_parent(parent, inst)
+            parent = self.ifc_info[ifc_parent]["ifc_inst"]
+            BlenderOperator().set_parent(inst, parent)
+
+    def set_transform(self):
+        for obj, ifc_info in self.ifc_info.items():
+            inst = self.ifc_info[obj]["ifc_inst"]
+            transform = self.ifc_info[obj]["transform"]
+            position = transform["position"]
+            rotation = transform["rotation"]
+            scale = transform["scale"]
+            BlenderOperator().set_location(inst, position)
+            BlenderOperator().set_rotation(inst, rotation)
+            BlenderOperator().set_scale(inst, scale)
 
     def execute(self):
         self.is_executed = True
@@ -237,39 +275,109 @@ class IFCBuilder:
 
         self.build()
         self.assemble()
+        self.set_transform()
         print("New IFC objects has been built.")
 
 
-if __name__ == "__main__":
-    ifc_parser = IFCParser("IfcBuilding/高雄港埠旅運中心")
-    ifc_parser.execute()
-    ifc_builder = IFCBuilder(ifc_parser)
-    ifc_builder.execute()
-
-
-class BlenderETree:
+class IFCETree:
     def __init__(self, builder: IFCBuilder):
         self.builder = builder
-        self.ifc_root = builder.ifc_root
-        self.ifc_type = builder.ifc_type
-        self.ifc_inherit = builder.ifc_inherit
-        self.ifc_objects = builder.ifc_objects
-        self.ifc_objects_partners = builder.ifc_objects_partners
+        self.ifc_info = builder.ifc_info
+        self.elements = {}
+        self.attributes = {}
+
+    class Element:
+        def __init__(self, obj=None):
+            self.obj = obj
+            self.attributes = {}
+            self.children = []
+
+        def set_attr(self, key, value):
+            self.attributes[key] = value
+            self.obj[key] = value
+
+        def remove_attr(self, key):
+            if key in self.attributes:
+                del self.attributes[key]
+                del self.obj[key]
+
+        def update_attr(self, key, new_value):
+            if key in self.attributes:
+                self.attributes[key] = new_value
+                self.obj[key] = new_value
+
+        def __repr__(self):
+            return f"<IFCElement '{self.obj.name}'>"
 
     def establish(self):
-        pass
+        for ifc_info in self.ifc_info.values():
+            obj = ifc_info["ifc_inst"]
+            self.elements[obj] = self.Element(bpy.data.objects.get(obj))
 
     def execute(self):
         if not self.builder.is_executed:
             return print("IFC builder has not been executed, would not build ETree.")
 
         self.establish()
+        for obj, ifc_elem in self.elements.items():
+            ifc_elem.set_attr("id", "my_custom_id")
+            # print(f"Element {obj} has been added to ETree, with element {ifc_elem}.")
 
+        print("IFC ETree elements has been set attributes.")
+
+        for obj, ifc_elem in self.elements.items():
+            ifc_elem.remove_attr("id")
+
+        print("IFC ETree elements has been removed attributes.")
+
+
+#
+#
+# class BlenderElement:
+#     def __init__(self, obj_name):
+#         self.obj = bpy.data.objects.get(obj_name)
+#         if self.obj is None:
+#             print(f"Object {obj_name} not found")
+#             return
+#         self.attributes = {}
+#
+#     def set_attr(self, key, value):
+#         self.attributes[key] = value
+#         self.obj[key] = value
+#
+#     def remove_attr(self, key):
+#         if key in self.attributes:
+#             del self.attributes[key]
+#             del self.obj[key]
+#
+#     def update_attr(self, key, new_value):
+#         if key in self.attributes:
+#             self.attributes[key] = new_value
+#             self.obj[key] = new_value
+#
+#     def __repr__(self):
+#         return f"<BlenderElement '{self.obj.name}'>"
+#
+#
+# # 使用範例
+# object_name = "IfcBuildingStorey/B2FL"
+# blender_element = BlenderElement(object_name)
+# blender_element.set_attr("id", "my_custom_id")
+#
+# print(blender_element.attributes)  # 輸出：{'id': 'my_custom_id'}
+# print(blender_element.obj["id"])  # 輸出：my_custom_id
+#
+# # 更新 id 屬性
+# blender_element.update_attr("id", "updated_id")
+#
+# # 移除 id 屬性
+# blender_element.remove_attr("id")
+# print(blender_element.attributes)  # 輸出：{'id': 'my_custom_id'}
 
 if __name__ == "__main__":
     ifc_parser = IFCParser("IfcBuilding/高雄港埠旅運中心")
     ifc_parser.execute()
     ifc_builder = IFCBuilder(ifc_parser)
     ifc_builder.execute()
-    blender_etree = BlenderETree(ifc_builder)
+    blender_etree = IFCETree(ifc_builder)
     blender_etree.execute()
